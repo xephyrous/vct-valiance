4f6f1cbceb926eaba410fa77628b959bd8d12b94
AlexanderYauchler <135302972+terminatedNULL@users.noreply.github.com>
2024-09-06 23:17:09 -0400

    VCT-Valiance :
    Features :
     + Implemented Chat Background
     + Implamented tentative Valiance animation
     + Started work on VCT Block
         - Needs animation tweaks
         - Also, the text is slightly left for som god forsaken unknown reason, good luck soldier o7

--- VCT-Valiance-Chatbot/composeApp/src/wasmJsMain/kotlin/org/xephyrous/com/App.kt
+++ VCT-Valiance-Chatbot/composeApp/src/wasmJsMain/kotlin/org/xephyrous/com/App.kt
@@ -2,24 +2,29 @@ package org.xephyrous.com
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.DpSize
 
 @Composable
 fun App() {
     MaterialTheme {
-        Box(
+        BoxWithConstraints(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color(0xFF181818)),
+                .background(Color(0xFF1E1E1E)),
         ) {
+            // Setup global size
+            Global.viewSize = DpSize(maxWidth, maxHeight)
+
+            // Splash screen
             ExpandableChatBackground()
-
-            VCTBlockText()
-
+            // VCTBlockText() TODO : FINISH
             Valiance()
         }
     }
