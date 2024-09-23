2239034eb9f8e6938adb9e8a4a6a67cf1e810672
AlexanderYauchler <135302972+terminatedNULL@users.noreply.github.com>
2024-09-23 17:41:56 -0400

    Refactored file structure

---
+++ VCT-Valiance-Chatbot/composeApp/src/wasmJsMain/kotlin/org/xephyrous/com/JSInterop/JSUtils.kt
@@ -0,0 +1,16 @@
+package org.xephyrous.com.JSInterop
+
+class TeamData(teamMessage: String) {
+    lateinit var name: String
+    lateinit var igl: String
+    lateinit var coach: String
+    lateinit var members: Array<String>
+    lateinit var roles: Array<String>
+    lateinit var agents: Array<String>
+    lateinit var theme: Array<String>
+}
+
+@JsModule("./js/js-utils.js")
+external class JSUtils {
+    
+}
