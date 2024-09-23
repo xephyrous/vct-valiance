04e36966e7464b00209707d1244bdb7fd960b822
AlexanderYauchler <135302972+terminatedNULL@users.noreply.github.com>
2024-09-23 17:38:59 -0400

    TeamData

--- VCT-Valiance-Chatbot/composeApp/src/wasmJsMain/kotlin/org/xephyrous/com/JSInterop/Firebase.kt
+++ VCT-Valiance-Chatbot/composeApp/src/wasmJsMain/kotlin/org/xephyrous/com/JSInterop/Firebase.kt
@@ -17,3 +17,13 @@ external object Firebase {
     fun addMessage(message: String, role: String) : Promise<JsAny>
     fun addTeam(teamObj: String) : Promise<JsAny>
 }
+
+class TeamData(teamMessage: String) {
+    lateinit var name: String
+    lateinit var igl: String
+    lateinit var coach: String
+    lateinit var members: Array<String>
+    lateinit var roles: Array<String>
+    lateinit var agents: Array<String>
+    lateinit var theme: Array<String>
+}
